044ac7c6a6f220a275d789df3a2cd357e8b8e9e6
文瑞 吕 <lvwenrui12@live.cn>
2020-07-26 20:44:52 +0800

    前端模块化

--- XiotFontEnd/src/app/app-routing.module.ts
+++ XiotFontEnd/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'device', loadChildren: '@modules/device-manage/device-manage.module#DeviceManageModule'
+},
+{
+  path: 'visualized', loadChildren: '@modules/visualized/visualized.module#VisualizedModule'
+},
+{
+  path: '',
+  redirectTo: 'device',
+  pathMatch: 'full'
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- XiotFontEnd/src/app/app.module.ts
+++ XiotFontEnd/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { DeviceManageModule } from '@modules/device-manage/device-manage.module';
+import { VisualizedModule } from '@modules/visualized/visualized.module'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    DeviceManageModule,
+    VisualizedModule,
+    AppRoutingModule,
+    RouterModule.forRoot([{
+      path: 'device', loadChildren: '@modules/device-manage/device-manage.module#DeviceManageModule'
+
+    },
+    {
+      path: 'visualized', loadChildren: './modules/visualized/visualized.module#VisualizedModule'
+
+    },
+    {
+      path: '',
+      redirectTo: 'device',
+      pathMatch: 'full'
+    }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
